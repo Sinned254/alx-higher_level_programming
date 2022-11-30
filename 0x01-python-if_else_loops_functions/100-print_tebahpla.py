ab6dc90b81c9b7947f4cd3a26929deec6a424524
Sinned254 <dennis1kosgei@gmail.com>
2022-11-30 10:27:11 -0800

    pycodestyle error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range (122, 96, -1):
+for i in range(122, 96, -1):
     if i % 2 == 0:
         c = i
     else:
         c = i - 32
     print("{:s}".format(chr(c)), end="")
-
