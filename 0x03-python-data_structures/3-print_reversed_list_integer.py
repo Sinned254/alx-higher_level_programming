868f0859cf09d613a0ed2733ad7b2892ec7a36f2
Sinned254 <dennis1kosgei@gmail.com>
2022-12-05 11:24:00 -0800

    syntax/pycode errros

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,3 @@ def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list)):
         newlist = my_list[::-1]
         print("{:d}".format(newlist[i]))
-
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     newlist = my_list[:]
     if idx < 0:
-        return(my_list[:])
+        return my_list[:]
     elif idx > len(my_list):
-        return(my_list[:])
+        return my_list[:]
     else:
         newlist[idx] = element
-        return(newlist)
+        return newlist
