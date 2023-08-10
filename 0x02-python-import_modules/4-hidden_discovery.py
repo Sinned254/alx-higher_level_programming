256cd4e57fb86a2542592211039e5bfe1619633b
Denis Kosgei <dennis1kosgei@gmail.com>
2023-08-10 11:44:47 -0700

    more push error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
-import hidden_4 as toprint
+if __name__ == "__main__":
+    import hidden_4 as toprint
 names = dir(toprint)
 for name in names:
     if not name.startwith("__"):
