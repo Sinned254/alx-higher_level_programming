fda8a4cb058be8dd2153fa4b2fbfcda542b3b594
Denis Kosgei <dennis1kosgei@gmail.com>
2023-10-19 00:50:43 -0700

    updates

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- List all shows without a genre
-SELECT CONCAT(tv_shows.title, ' - ', tv_show_genres.genre_id) AS 'Show and Genre'
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
